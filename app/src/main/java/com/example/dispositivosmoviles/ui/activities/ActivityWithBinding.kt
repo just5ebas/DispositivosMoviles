9f01ba70e31a333c26a72020cf796af94e530a8b
Ariel Maldonado <arielsebas2000@gmail.com>
2023-06-07 10:28:32 -0500

    Barra de navegacion

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/ActivityWithBinding.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/ActivityWithBinding.kt
@@ -2,11 +2,14 @@ package com.example.dispositivosmoviles.ui.activities
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.dispositivosmoviles.R
 
 import com.example.dispositivosmoviles.databinding.ActivityWithBindingBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ActivityWithBinding : AppCompatActivity() {
 
@@ -27,11 +30,11 @@ class ActivityWithBinding : AppCompatActivity() {
         var name: String = ""
 
         // De esta forma accedemos a los contenidos enviados por otra Activity
-        intent.extras.let {
-            // it? significa que este item/objeto puede ser nulo
-            name = it?.getString("var1")!!
-        }
-        Log.d("UCE", "Hola $name")
+//        intent.extras.let {
+//            // it? significa que este item/objeto puede ser nulo
+//            name = it?.getString("var1")!!
+//        }
+//        Log.d("UCE", "Hola $name")
 
         binding.textView.text = "Bienvenido $name!"
 
@@ -67,6 +70,49 @@ class ActivityWithBinding : AppCompatActivity() {
             // Con esto iniciamos el otro Activity
             startActivity(intent)
 
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    var suma:Int = 0
+                    for(i in 1..10){
+                        suma += i
+                    }
+                    Snackbar.make(binding.textView,
+                        "La suma es $suma",
+                        Snackbar.LENGTH_LONG
+                    ).setBackgroundTint(Color.rgb(232, 87, 87)).show()
+
+                    true
+                }
+                R.id.fav -> {
+                    var prod:Int = 1
+                    for(i in 1..10){
+                        prod *= i
+                    }
+                    Snackbar.make(binding.textView,
+                        "El producto es $prod",
+                        Snackbar.LENGTH_LONG
+                    ).setBackgroundTint(Color.rgb(29, 167, 242)).show()
+
+                    true
+                }
+                R.id.chat_gpt -> {
+                    var suma:Int = 0
+                    for(i in 1..10){
+                        suma += i
+                    }
+                    Snackbar.make(binding.textView,
+                        "La suma es $suma",
+                        Snackbar.LENGTH_LONG
+                    ).setBackgroundTint(Color.rgb(24, 119, 242)).show()
+
+                    true
+                }
+
+                else -> {false}
+            }
         }
     }
 
